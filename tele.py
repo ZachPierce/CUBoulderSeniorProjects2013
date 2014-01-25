5edf35ff9bda8736f14ae7f2ab8570be5061ae3f
nathanielpohl <nathaniel.pohl@colorado.edu>
2014-01-25 02:11:17 +0000

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,59 +1,50 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 import sys
 
+blueLED =  open('/dev/talos/led/blue','w', 0)
+greenLED = open('/dev/talos/led/green','w', 0)
+redLED = open('/dev/talos/led/red', 'w', 0)
+yellowLED = open('/dev/talos/led/yellow', 'w', 0)
 
-blueLED = open ('/dev/talos/led/blue', 'w')
-greenLED = open ('/dev/talos/led/green','w')
-redLED = open ('/dev/talos/led/red', 'w')
-yellowLED = open ('/dev/talos/led/yellow', 'w')
+leftFront = open('/dev/talos/motorF/speed', 'w', 0)
+leftBack = open('/dev/talos/motorE/speed', 'w', 0)
+rightFront = open('/dev/talos/motorA/speed', 'w', 0)
+rightBack = open('/dev/talos/motorB/speed', 'w', 0)
 
-#leftMotor = open('/dev/talos/motorF/speed', 'w')
-#rightMotor = open('/dev/talos/motorA/speed', 'w')
+joyR = open('/dev/talos/joystick1/axis3', 'r')
+joyL = open('/dev/talos/joystick1/axis1', 'r')
 
+buttonA = open('/dev/talos/joystick1/button0', 'r')
+buttonB = open('/dev/talos/joystick1/button1', 'r')
 
-# TODO: Some timing weirdness with not capturing all
-# joystick input.
+rightFlipper = open('/dev/talos/servo1/position', 'w', 0)
+leftFlipper = open('/dev/talos/servo4/position', 'w', 0)
+
 def capture():
 	pipe = open('/dev/input/js0','r')
-	action = []
-	spacing = 0
 	while 1:
-		for character in pipe.read(1):
-			action += ['%02X' % ord(character)]
-			if len(action) == 8:
-	
-				num = int(action[5], 16) # Translate back to integer form
+		btnA = buttonA.readline()
+		btnB = buttonB.readline()
+		if btnA == '1\n':
+			greenLED.write('1')
+			leftFlipper.write('255') # fully out
+		elif btnA == '0\n':
+			greenLED.write('0')
+			leftFlipper.write('1') # fully in
+		if btnB == '1\n':
+			redLED.write('1')
+			rightFlipper.write('1') # fully out
+		elif btnB == '0\n':
+			redLED.write('0')
+			rightFlipper.write('255') # fully in
 
-				if action[6] == '01': # Button
-					if action[4] == '01':
-						buttonPress(action[7])
-					else:
-						buttonRelease(action[7])
-
-				elif action[7] == '00': # Left Joystick left/right
-					leftJoyX(num)
-
-				elif action[7] == '01': # Left Joystick up/down
-					leftJoyY(num)
-
-				elif action[7] == '04': # D-pad left/right
-					dPadX(action[4])
-
-				elif action[7] == '05': # D-pad up/down
-					dPadY(action[4])
-
-				elif action[7] == '02': # Right Joystick left/right
-					rightJoyX(num)
-
-				elif action[7] == '03': # Right Joystick up/ down
-					rightJoyY(num)
-				action = []
-
-
+		leftJoyY(joyL.readline())
+		rightJoyY(joyR.readline())
+		print buttonA.readline()
+		print buttonB.readline()
 
 # Tank drive with the two joysticks.
-# Make sure to flush the output, or else python wont do
-# anything until the program ends.
+'''
 def buttonPress(btn):
 	# 00 -> x button
 	# 01 -> a button
@@ -68,42 +59,38 @@ def buttonPress(btn):
 	# 0A -> right joy press
 	# 0B -> left joy press
 
-	if btn == '00':
+	if btn == '02':
 		blueLED.write('1')
-		blueLED.flush()
-	elif btn == '01':
+	elif btn == '00':
 		greenLED.write('1')
-		greenLED.flush()
-	elif btn == '02':
+	elif btn == '01':
 		redLED.write('1')
-		redLED.flush()
 	elif btn == '03':
 		yellowLED.write('1')
-		yellowLED.flush()
 
 
 def buttonRelease(btn):
-	if btn == '00':
+	if btn == '02':
 		blueLED.write('0')
-		blueLED.flush()
-	elif btn == '01':
+	elif btn == '00':
 		greenLED.write('0')
-		greenLED.flush()
-	elif btn == '02':
+	elif btn == '01':
 		redLED.write('0')
-		redLED.flush()
 	elif btn == '03':
 		yellowLED.write('0')
-		yellowLED.flush()
+'''
 
 def leftJoyX(num):
-	return # tank drive, disregard changes in x.
+	return
 
 def leftJoyY(num):
-	return
-	# no motors to work with
-	leftMotor.write(num)
-	leftMotor.flush()
+	s = str(num) # converting to str might be unnessacary.
+	if s[0] == '-':
+		leftFront.write(s[1:])
+		leftBack.write(s[1:])
+	else:
+		leftFront.write('-'+s)
+		leftBack.write('-'+s)
 
 def dPadX(x):
 	if x == '01':
@@ -121,10 +108,13 @@ def rightJoyX(num):
 	return # tank drive, disregard changes in x.
 
 def rightJoyY(num):
-	return
-	# no motors to work with
-	rightMotor.write(num)
-	rightMotor.flush()
+	s = str(num)
+	if s[0] == '-':
+		rightFront.write(s[1:])
+		rightBack.write(s[1:])
+	else:
+		rightFront.write('-'+s)
+		rightBack.write('-'+s)
 
 
 capture()
