e3a33fdef21e1a461739619af101e937335b1c63
nathanielpohl <nathaniel.pohl@colorado.edu>
2014-02-05 04:47:29 +0000

    Added local logging system

--- auto.py
+++ auto.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python 
 import sys
 import time
+import datetime
+import atexit
 
 
 blueLED = open ('/dev/talos/led/blue', 'w', 0)
@@ -10,8 +12,28 @@ yellowLED = open ('/dev/talos/led/yellow', 'w', 0)
 
 buzzer = open('/dev/talos/buzzer', 'w', 0)
 
+st = datetime.datetime.fromtimestamp(time.time()).strftime('auto_%Y-%m-%d_%H:%M:%S')
+logfh = open(st, 'w+')
 
+def log(msg):
+	st = datetime.datetime.fromtimestamp(time.time()).strftime('[%Y-%m-%d %H:%M:%S] ')
+	logfh.write(st + msg + '\n')
+
+@atexit.register
+def end():
+	blueLED.close()
+	greenLED.close()
+	redLED.close()
+	yellowLED.close()
+	buzzer.close()
+	log('Exiting Autonomuous Code')
+	logfh.close()
+
+
+
+log('Starting Autonomous Code')
 while True:
+	log('B and G on  ... R and Y off')
 	blueLED.write(str(1))
 	greenLED.write(str(1))
 	redLED.write(str(0))
@@ -19,6 +41,7 @@ while True:
 
 	time.sleep(1)
 	
+	log('B and G off ... R and Y on')
 	blueLED.write(str(0))
 	greenLED.write(str(0))	
 	redLED.write(str(1))	
