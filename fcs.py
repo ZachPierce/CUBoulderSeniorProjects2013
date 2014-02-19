d871be6ce3c633273bed2adb1dcad715edfcc742
nathanielpohl <nathaniel.pohl@colorado.edu>
2014-02-19 03:06:55 +0000

    Fixed killall

--- fcs.py
+++ fcs.py
@@ -37,21 +37,21 @@ log('Starting the fcs')
 def main():
     log('Parsing the config file')
     parseConfig()
-    killAll()
+    os.system("/home/talos/killall")
     log('Cleared the motor and LED states')
     log('Waiting for the user to trigger the autonomuous code')
     waitForStart()
     log('Autonomuos code started')
     runCode(mode, autoTime, autoCode)
     log('Autonomous code ended')
-    killAll()
+    os.system("/home/talos/killall")
     log('Cleared the motor and LED states')
     log('Waiting for the user to trigger the tele-op code')
     waitForStart()
     log('Tele-op code started')
     runCode(mode, teleTime, teleCode)
     log('Tele-op code ended')
-    killAll()
+    os.system("/home/talos/killall")
     log('Cleared the motor and LED states')
 
 
@@ -87,45 +87,6 @@ def parseConfig():
     if (teleTime < 0):
         print 'Your tele-op time is  less than zero, watchout!'
         teleTime = 0
-
-
-
-def killAll(): # this function still needs some works.
-    mA = open('/dev/talos/motorA/speed', 'w', 0)
-    mB = open('/dev/talos/motorB/speed', 'w', 0)
-    mC = open('/dev/talos/motorC/speed', 'w', 0)
-    mD = open('/dev/talos/motorD/speed', 'w', 0)
-    mE = open('/dev/talos/motorE/speed', 'w', 0)
-    mF = open('/dev/talos/motorF/speed', 'w', 0)
-    mA.write('0')
-    mB.write('0')
-    mC.write('0')
-    mD.write('0')
-    mE.write('0')
-    mF.write('0')
-    mA.close()
-    mB.close()
-    mC.close()
-    mD.close()
-    mE.close()
-    mF.close()
-
-    bz = open ('/dev/talos/buzzer', 'w', 0)
-    bz.write('0')
-    bz.close()
-
-    lr = open('/dev/talos/led/red', 'w', 0)
-    lg = open('/dev/talos/led/green', 'w', 0)
-    lb = open('/dev/talos/led/blue', 'w', 0)
-    ly = open('/dev/talos/led/yellow', 'w', 0)
-    lr.write('0')
-    lg.write('0')
-    lb.write('0')
-    ly.write('0')
-    lr.close()
-    lg.close()
-    lb.close()
-    ly.close()
 
 
 
