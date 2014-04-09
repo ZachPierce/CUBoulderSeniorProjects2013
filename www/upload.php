a230c4d780e115d49f83ab56a47a16c443037ac2
nathanielpohl <nathaniel.pohl@colorado.edu>
2014-04-09 12:46:11 -0600

    Create upload.php

---
+++ www/upload.php
@@ -0,0 +1,22 @@
+<?php
+function check_file_uploaded_name ($filename)
+{
+    (bool) ((preg_match("`^[-0-9A-Z_\.]+$`i",$filename)) ? true : false);
+}
+
+if ($_FILES["file"]["error"] > 0){
+        echo "Error: " . $_FILES["file"]["error"] . "<br>";
+}
+else{
+        echo "Upload: " . $_FILES["userfile"]["name"] . "<br>";
+        echo "Type: " . $_FILES["file"]["type"] . "<br>";
+        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        echo "Temporarily Stored in: " . $_FILES["file"]["tmp_name"] . "<br>";
+        if (!move_uploaded_file($_FILES["file"]["tmp_name"], "code/" . $_FILES["file"]["name"])) {
+                echo "<br>failed to copy " . $_FILES['file']['name']."...";
+        }
+        else{
+                echo "<br><br>Stored in: " . "/var/www/code/" . $_FILES["file"]["name"];
+        }
+}
+?>
