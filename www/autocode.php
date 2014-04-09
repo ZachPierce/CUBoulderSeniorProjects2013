bb2411b5a696c0aecc7d030300b226fd76a0c22f
nathanielpohl <nathaniel.pohl@colorado.edu>
2014-04-09 12:32:40 -0600

    Create autocode.php

---
+++ www/autocode.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="autoupload.php" method="post"
+enctype="multipart/form-data">
+<label for="file">Autonomous Code Filename:</label>
+<input type="file" name="file" id="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
