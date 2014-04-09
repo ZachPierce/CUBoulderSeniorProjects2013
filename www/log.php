681068ac5e205f5339ef11f309206b57fa38e76c
nathanielpohl <nathaniel.pohl@colorado.edu>
2014-04-09 12:34:57 -0600

    Update log.php

--- www/log.php
+++ www/log.php
@@ -1,7 +1,7 @@
 <?php
 if ($handle = opendir('/var/www/logs/')) {
     while (false !== ($entry = readdir($handle))) {
-        if (substr($entry, 0, 4) == 'fcs_') {
+        if (substr($entry, 0, 4) == 'fcs_' || substr($entry, 0, 5) == 'tele_') {
             echo "<a href='./logs/$entry'>$entry</a><br>";
         }
     }
